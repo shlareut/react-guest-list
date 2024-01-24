e361b5ae48e9a3ff71b90917ca87295d108d13d1
Lars Reuter <lp.reuter@gmail.com>
2024-01-24 20:15:29 +0100

    Add guest table structure and local functionality

--- src/FormfieldComponent.js
+++ src/FormfieldComponent.js
@@ -13,39 +13,52 @@ export default function FormfieldComponent() {
   // }, [guests]);
 
   return (
-    <div className={styles.formfields}>
-      <label htmlFor="firstName">First name</label>
-      <input
-        name="firstName"
-        id="firstName"
-        onChange={(event) => {
-          const newFirstName = event.currentTarget.value;
-          setFirstName(newFirstName);
-        }}
-      />
-      <label htmlFor="lastName">Last name</label>
-      <input
-        name="lastName"
-        id="lastName"
-        onChange={(event) => {
-          const newLastName = event.currentTarget.value;
-          setLastName(newLastName);
-        }}
-        onKeyDown={(event) => {
-          if (event.key === 'Enter') {
-            const newGuests = [...guests];
-            newGuests.push({ firstName: firstName, lastName: lastName });
-            setGuests(newGuests);
-          }
-        }}
-      />
-      <p>{firstName}</p>
-      <p>{lastName}</p>
-      <ul>
-        {guests.map((guest) => (
-          <li key={lastName}>{`${guest.firstName} ${guest.lastName}`}</li>
-        ))}
-      </ul>
-    </div>
+    <>
+      <div className={styles.formfields}>
+        <label htmlFor="firstName">First name</label>
+        <input
+          name="firstName"
+          id="firstName"
+          onChange={(event) => {
+            const newFirstName = event.currentTarget.value;
+            setFirstName(newFirstName);
+          }}
+        />
+        <label htmlFor="lastName">Last name</label>
+        <input
+          name="lastName"
+          id="lastName"
+          onChange={(event) => {
+            const newLastName = event.currentTarget.value;
+            setLastName(newLastName);
+          }}
+          onKeyDown={(event) => {
+            if (event.key === 'Enter') {
+              const newGuests = [...guests];
+              newGuests.push({ firstName: firstName, lastName: lastName });
+              setGuests(newGuests);
+            }
+          }}
+        />
+      </div>
+      <div>
+        <div className={styles.guestTable}>
+          <tr>
+            <th>Attendance</th>
+            <th>First Name</th>
+            <th>Last Name</th>
+          </tr>
+          {guests.map((guest) => (
+            <tr key={guest.lastName}>
+              <td>
+                <input type="checkbox" />
+              </td>
+              <td>{guest.firstName}</td>
+              <td>{guest.lastName}</td>
+            </tr>
+          ))}
+        </div>
+      </div>
+    </>
   );
 }
