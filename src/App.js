e1fba4a63a6b0beba234412f9857c311d25a83cf
Lars Reuter <lp.reuter@gmail.com>
2024-01-25 23:38:07 +0100

    Revert to components

--- src/App.js
+++ src/App.js
@@ -1,17 +1,16 @@
 import './App.module.scss';
 import { useState } from 'react';
-import InputAndTable from './InputAndTable';
 import InputFieldComponent from './InputFieldComponent';
 import TableComponent from './TableComponent';
 
 export default function App() {
-  // const baseUrl = 'http://localhost:4000';
-  // const [guests, setGuests] = useState([]);
-  // const [isLoading, setIsLoading] = useState(true);
-  // const [isDisabled, setIsDisabled] = useState(true);
+  const baseUrl = 'http://localhost:4000';
+  const [guests, setGuests] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [isDisabled, setIsDisabled] = useState(true);
   return (
     <div>
-      {/* <InputFieldComponent isDisabled={isDisabled} />
+      <InputFieldComponent isDisabled={isDisabled} />
       <TableComponent
         baseUrl={baseUrl}
         guests={guests}
@@ -19,8 +18,7 @@ export default function App() {
         isLoading={isLoading}
         setIsLoading={setIsLoading}
         setIsDisabled={setIsDisabled}
-      /> */}
-      <InputAndTable />
+      />
     </div>
   );
 }
