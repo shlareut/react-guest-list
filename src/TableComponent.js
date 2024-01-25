8826842b31668e88e1c4a3b3b5f07ffac4a2cbb5
Lars Reuter <lp.reuter@gmail.com>
2024-01-25 23:25:55 +0100

    Fix drone issue

--- src/TableComponent.js
+++ src/TableComponent.js
@@ -3,40 +3,41 @@ import React, { useEffect } from 'react';
 export default function TableComponent(props) {
   useEffect(() => {
     const getGuests = async () => {
-      props.setIsLoading(true);
-      props.setIsDisabled(true);
       const response = await fetch(`${props.baseUrl}/guests`);
       const allGuests = await response.json();
       props.setGuests(allGuests);
+      props.setIsLoading(false);
     };
     getGuests().catch((error) => {
       console.log(error);
     });
-    props.setIsDisabled(false);
-    props.setIsLoading(false);
   }, []);
+  // Should try to place useEffect into App.js and only trigger on initial render // or alternatively I should create all functions first and then see if the error still persists, maybe it fixes something
   if (props.isLoading) {
     return 'Loading...';
   }
   return (
-    <table>
-      <thead>
-        <tr>
-          <th>ID</th>
-          <th>First Name</th>
-          <th>Last Name</th>
-          <th>Attending</th>
-        </tr>
-      </thead>
-      <tbody>
-        {props.guests.map((person) => (
-          <tr key={`ID${person.id}`} data-test-id="guest">
-            <td>{person.id}</td>
-            <td>{person.firstName}</td>
-            <td>{person.lastName}</td>
+    <>
+      {props.setIsDisabled(false)}
+      <table>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Attending</th>
           </tr>
-        ))}
-      </tbody>
-    </table>
+        </thead>
+        <tbody>
+          {props.guests.map((person) => (
+            <tr key={`ID${person.id}`} data-test-id="guest">
+              <td>{person.id}</td>
+              <td>{person.firstName}</td>
+              <td>{person.lastName}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </>
   );
 }
