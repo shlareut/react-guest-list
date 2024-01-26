8f1270accf6b533c39e90e4d2f9b27a1bd31a7d9
Lars Reuter <lp.reuter@gmail.com>
2024-01-26 08:53:17 +0100

    Fix drone issue

--- src/TableComponent.js
+++ src/TableComponent.js
@@ -3,11 +3,11 @@ import React, { useEffect } from 'react';
 export default function TableComponent(props) {
   useEffect(() => {
     const getGuests = async () => {
-      props.setIsDisabled(false);
-      props.setIsLoading(false);
       const response = await fetch(`${props.baseUrl}/guests`);
       const allGuests = await response.json();
       props.setGuests(allGuests);
+      props.setIsDisabled(false);
+      props.setIsLoading(false);
     };
     getGuests().catch((error) => {
       console.log(error);
@@ -18,24 +18,12 @@ export default function TableComponent(props) {
     return 'Loading...';
   }
   return (
-    <table>
-      <thead>
-        <tr>
-          <th>ID</th>
-          <th>First Name</th>
-          <th>Last Name</th>
-          <th>Attending</th>
-        </tr>
-      </thead>
-      <tbody>
-        {props.guests.map((person) => (
-          <tr key={`ID${person.id}`} data-test-id="guest">
-            <td>{person.id}</td>
-            <td>{person.firstName}</td>
-            <td>{person.lastName}</td>
-          </tr>
-        ))}
-      </tbody>
-    </table>
+    <ul>
+      {props.guests.map((person) => (
+        <li key={`ID${person.id}`} data-test-id="guest">
+          {person.firstName} {person.lastName}
+        </li>
+      ))}
+    </ul>
   );
 }
