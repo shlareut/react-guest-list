462f57129d80018d8a66d8286e4fca773917eef8
Lars Reuter <lp.reuter@gmail.com>
2024-01-26 17:19:06 +0100

    Refactor

--- src/App.js
+++ src/App.js
@@ -36,6 +36,9 @@ export default function App() {
       body: JSON.stringify({ firstName: firstName, lastName: lastName }),
     });
     const createdGuest = await response.json();
+    const newGuests = [...guests];
+    newGuests.push(createdGuest);
+    setGuests(newGuests);
   }
   // 3. Async function to delete user
   async function deleteGuest(guestId) {
@@ -89,16 +92,16 @@ export default function App() {
             }}
             onKeyDown={(event) => {
               if (event.key === 'Enter') {
-                const newGuests = [...guests];
-                const lastGuestIndex = newGuests.length - 1;
-                const lastGuestId = guests[lastGuestIndex].id;
-                newGuests.push({
-                  id: +lastGuestId + 1,
-                  firstName: firstName,
-                  lastName: lastName,
-                  attending: false,
-                });
-                setGuests(newGuests);
+                // const newGuests = [...guests];
+                // const lastGuestIndex = newGuests.length - 1;
+                // const lastGuestId = guests[lastGuestIndex].id;
+                // newGuests.push({
+                //   id: +lastGuestId + 1,
+                //   firstName: firstName,
+                //   lastName: lastName,
+                //   attending: false,
+                // });
+                // setGuests(newGuests);
                 createGuest().catch((error) => {
                   console.log(error);
                 });
